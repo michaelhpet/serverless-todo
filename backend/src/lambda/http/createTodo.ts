516dc15479daaedc41848080839541b62f114d09
Michael Peter <michaelhpet@gmail.com>
2023-03-18 22:40:32 +0100

    feat: implemented createTodo handler

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,15 +3,31 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import { createTodo } from '../../helpers/business_logic'
+import { TodoItem } from '../../models/TodoItem'
+
+const logger = createLogger('createTodo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
+    // TODO: Implement creating a new TODO item => DONE
+    logger.info('Processing a new event: createTodo', { event })
 
-    return undefined
+    const userId: string = getUserId(event)
+    const newTodoItem: TodoItem = await createTodo(userId, newTodo)
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({ item: newTodoItem })
+    }
+  }
 )
 
 handler.use(
