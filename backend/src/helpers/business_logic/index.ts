cd6b539ba3d48def27c442ca311c8a431afeb100
Michael Peter <michaelhpet@gmail.com>
2023-03-18 22:40:32 +0100

    feat: wrote business logic and data layer

---
+++ backend/src/helpers/business_logic/index.ts
@@ -0,0 +1,130 @@
+import * as uuid from 'uuid'
+import {
+  createTodoItem,
+  deleteTodoItem,
+  getTodoItem,
+  getTodosByUserId,
+  updateTodoItem,
+  updateAttachmentUrl as _updateAttachmentUrl
+} from '../data_layer'
+import { getAttachmentUrl, getSignedUrl } from '../file_storage'
+import { TodoItem } from '../../models/TodoItem'
+import { createLogger } from '../../utils/logger'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { TodoUpdate } from '../../models/TodoUpdate'
+
+// TODO: Implement businessLogic => DONE
+const logger = createLogger('businessLogic-todos')
+
+export async function createTodo(
+  userId: string,
+  payload: CreateTodoRequest
+): Promise<TodoItem> {
+  const todoId: string = uuid.v4()
+
+  const todoItem: TodoItem = {
+    userId,
+    todoId,
+    createdAt: new Date().toISOString(),
+    done: false,
+    attachmentUrl: null,
+    ...payload
+  }
+
+  try {
+    await createTodoItem(todoItem)
+    logger.info(`Todo item created successfully: ${todoId}`, todoItem)
+    return todoItem
+  } catch (error) {
+    logger.error(`Could not create todo item for user: ${userId}`)
+  }
+}
+
+export async function getTodos(userId: string): Promise<TodoItem[]> {
+  try {
+    const todoItems = await getTodosByUserId(userId)
+    logger.info(
+      `Fetched todo items for user: ${userId}`,
+      JSON.stringify(todoItems)
+    )
+    return todoItems
+  } catch (error) {
+    logger.error(`Could not fetch todo items for user: ${userId}`)
+    return []
+  }
+}
+
+export async function updateTodo(
+  userId: string,
+  todoId: string,
+  payload: UpdateTodoRequest
+): Promise<void> {
+  try {
+    const item = await getTodoItem(todoId)
+
+    if (!item) throw new Error('Todo item not found')
+
+    if (userId !== item.userId) {
+      throw new Error('Update action on todo item unauthorized')
+    }
+
+    await updateTodoItem(todoId, payload as TodoUpdate)
+    logger.info('Todo item updated:', payload)
+  } catch (error) {
+    logger.error(error?.message || 'Could not update todo item')
+  }
+}
+
+export async function deleteTodo(
+  userId: string,
+  todoId: string
+): Promise<void> {
+  try {
+    const item = await getTodoItem(todoId)
+
+    if (!item) throw new Error('Todo item not found')
+
+    if (userId !== item.userId) {
+      throw new Error('Delete action on todo item unauthorized')
+    }
+
+    await deleteTodoItem(todoId)
+    logger.info(`Deleted todo item: ${todoId} for user: ${userId}`)
+  } catch (error) {
+    logger.error(error?.message || 'Could not delete todo item')
+  }
+}
+
+export async function updateAttachmentUrl(
+  userId: string,
+  todoId: string,
+  attachmentId: string
+): Promise<void> {
+  try {
+    const attachmentUrl = getAttachmentUrl(attachmentId)
+
+    const item = await getTodoItem(todoId)
+
+    if (!item) throw new Error('Todo item not found')
+
+    if (userId !== item.userId) {
+      throw new Error('Update action on todo item unauthorized')
+    }
+
+    await _updateAttachmentUrl(todoId, attachmentUrl)
+    logger.info(`Updated todo item attachment url: ${todoId}, ${attachmentUrl}`)
+  } catch (error) {
+    logger.error(error?.message || 'Could not update attachment URL')
+  }
+}
+
+export function getUploadUrl(attachmentId: string): string {
+  try {
+    const signedUrl = getSignedUrl(attachmentId)
+    logger.info(`Signed URL created: ${signedUrl}`)
+    return signedUrl
+  } catch (error) {
+    logger.error('Could not get upload URL')
+  }
+}
---
+++ backend/src/helpers/data_layer/index.ts
@@ -0,0 +1,113 @@
+import 'source-map-support/register'
+
+import * as AWS from 'aws-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../../utils/logger'
+import { TodoItem } from '../../models/TodoItem'
+import { TodoUpdate } from '../../models/TodoUpdate'
+
+const AWSXRay = require('aws-xray-sdk')
+
+const logger = createLogger('dataLayer-todos')
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const todosByUserIndex = process.env.TODOS_BY_USER_INDEX
+
+export async function createTodoItem(todoItem: TodoItem): Promise<void> {
+  await docClient
+    .put({
+      TableName: todosTable,
+      Item: todoItem
+    })
+    .promise()
+
+  logger.info(`Todo item ${todoItem.todoId} was created`)
+}
+
+export async function getTodoItem(todoItemId: string): Promise<TodoItem> {
+  const result = await docClient
+    .get({
+      TableName: todosTable,
+      Key: { todoItemId }
+    })
+    .promise()
+
+  logger.info(`Todo item fetched: ${result.Item}`)
+  return result.Item as TodoItem
+}
+
+export async function getTodosByUserId(userId: string): Promise<TodoItem[]> {
+  const result = await docClient
+    .query({
+      TableName: todosTable,
+      IndexName: todosByUserIndex,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId
+      }
+    })
+    .promise()
+
+  logger.info(`Todos successfully fetched for user: ${userId}`)
+  return result.Items as TodoItem[]
+}
+
+export async function todoItemExists(todoItemId: string): Promise<boolean> {
+  const todoItem = await getTodoItem(todoItemId)
+  return Boolean(todoItem)
+}
+
+export async function updateTodoItem(
+  todoItemId: string,
+  payload: TodoUpdate
+): Promise<void> {
+  await docClient
+    .update({
+      TableName: todosTable,
+      Key: { todoItemId },
+      UpdateExpression: 'set #name = :name, dueDate = :dueDate, done = :done',
+      ExpressionAttributeNames: {
+        '#name': 'name'
+      },
+      ExpressionAttributeValues: {
+        ':name': payload.name,
+        ':dueDate': payload.dueDate,
+        ':done': payload.done
+      }
+    })
+    .promise()
+
+  logger.info(`Todo item updated successfully: ${todoItemId}`)
+}
+
+export async function updateAttachmentUrl(
+  todoItemId: string,
+  attachmentUrl: string
+): Promise<void> {
+  await docClient
+    .update({
+      TableName: todosTable,
+      Key: { todoItemId },
+      UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+      ExpressionAttributeValues: {
+        ':attachmentUrl': attachmentUrl
+      }
+    })
+    .promise()
+
+  logger.info(`Todo item attachment URL updated: ${todoItemId}`)
+}
+
+export async function deleteTodoItem(todoItemId: string): Promise<void> {
+  await docClient
+    .delete({
+      TableName: todosTable,
+      Key: { todoItemId }
+    })
+    .promise()
+
+  logger.info(`Todo item deleted successfully: ${todoItemId}`)
+}
