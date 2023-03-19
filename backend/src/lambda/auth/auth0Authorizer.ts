1358d59a4f660bd764838f665e9a9c570ede014a
Michael Peter <michaelhpet@gmail.com>
2023-03-19 09:13:19 +0100

    fix: domain for jwksUrl

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = `https://${process.env.AUTH0_DOMAIN_ID}/.well-known/jwks.json`
+const jwksUrl: string = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
 
 let cachedCertificate: string
 
